006312971fb492922262c3332d4e16aa4cc624e2
realkevinkwon <kevinkwonmail@gmail.com>
2022-12-22 13:18:32 -0500

    dataset complete

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from model import KnotClassifier
+from dataset import Knots
+from torchvision import transforms
 
 def main():
-    pass
+    transform = transforms.Compose([transforms.CenterCrop(3456), transforms.Resize((256,256))])
+
+    knots_data = Knots(transform=transform)
+    img, target = knots_data.__getitem__(683)
+    img.show()
 
 if __name__ == '__main__':
     main()
