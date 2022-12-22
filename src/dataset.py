8fd37234456bdf84e9e87db80aa83471a20447d4
realkevinkwon <kevinkwonmail@gmail.com>
2022-12-22 12:57:31 -0500

    add code for getting image filepaths

--- src/dataset.py
+++ src/dataset.py
@@ -1,25 +1,38 @@
-from torch.utils.data import Dataset
+import os
+from torchvision.datasets.vision import VisionDataset
+from PIL import Image
 
-class Knots(Dataset):
 
-    def __init__(self, root, train, transform=None, target_transform=None):
-        self.root = root
-        self.train = train
+class Knots(VisionDataset):
+
+    def __init__(self, transform=None):
+        self.root = './data/'
         self.transform = transform
-        self.data = []
+        self.filepaths = []
         self.targets = []
+
+        super().__init__(self.root, transforms=None, transform=transform)
+
+        classes = []
+        for item in os.listdir(os.path.join(self.root, '10Knots')):
+            if item != '.DS_Store':
+                classes.append(item)
+        
+        for label in classes:
+            for path, _, filenames in os.walk(os.path.join(self.root, '10Knots', label)):
+                for filename in filenames:
+                    if filename != '.DS_Store':
+                        self.filepaths.append(os.path.join(path, filename))
+                        self.targets.append(label)
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
-
-        img, target = self.data[idx], self.targets[idx]
+        img = Image.open(self.filepaths[idx])
+        target = self.targets[idx]
 
         if self.transform is not None:
             img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
 
         return img, target
